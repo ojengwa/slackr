2aab36ce686537bc95e50d93dadc555342f0da5a
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-02-12 01:22:54 +0100

    create test bootstrap

---
+++ test/bootstrap.test.js
@@ -0,0 +1,24 @@
+var sails = require('sails');
+
+before(function (done) {
+    'use strict';
+
+    // Increase the Mocha timeout so that Sails has enough time to lift.
+    this.timeout(5000);
+
+    sails.lift({
+        // configuration for testing purposes
+    }, function (err, server) {
+        if (err) {
+            return done(err);
+        }
+        // here you can load fixtures, etc.
+        done(err, sails);
+    });
+});
+
+after(function (done) {
+    'use strict';
+    // here you can clear fixtures, etc.
+    sails.lower(done);
+});
